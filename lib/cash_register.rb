a0a35035e401f36502414a27b6ca5b18a53cf8af
derrick b <dbozich1@gmail.com>
2018-05-31 02:42:54 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,70 +1,71 @@
-class CashRegister
-
-  attr_accessor :items, :discount, :total, :last_transaction
-
-  def initialize(discount=0)
-    @total = 0
-    @discount = discount
-    @items = []
-  end
-
-  def add_item(title, amount, quantity=1)
-    self.total += amount * quantity
-    quantity.times do
-      items << title
-    end
-    self.last_transaction = amount * quantity
-  end
-
-  def apply_discount
-    if discount != 0
-      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
-      "After the discount, the total comes to $#{self.total}."
-    else
-      "There is no discount to apply."
-    end
-  end
-
-  def void_last_transaction
-    self.total = self.total - self.last_transaction
-  end
-end
-
-
 # class CashRegister
-#   attr_accessor :total, :discount, :items, :last_item
 #
-#   def initialize(discount = 0)
+#   attr_accessor :items, :discount, :total, :last_transaction
+#
+#   def initialize(discount=0)
 #     @total = 0
 #     @discount = discount
 #     @items = []
 #   end
 #
-#
-#   def add_item(title, price, quantity = 1)
-#     self.total += (price * quantity)
+#   def add_item(title, amount, quantity=1)
+#     self.total += amount * quantity
 #     quantity.times do
-#       self.items.push(title)
+#       items << title
 #     end
-#     self.last_item = (price * quantity)
+#     self.last_transaction = amount * quantity
 #   end
 #
 #   def apply_discount
-#     if self.discount > 0
-#     self.total -= self.discount
-#     "After the discount, the total comes to $#{self.total}."
+#     if discount != 0
+#       self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+#       "After the discount, the total comes to $#{self.total}."
 #     else
 #       "There is no discount to apply."
 #     end
 #   end
 #
-#
-#
 #   def void_last_transaction
-#     self.total -= self.last_item
-#
+#     self.total = self.total - self.last_transaction
 #   end
 # end
+
+
+class CashRegister
+  attr_accessor :total, :discount, :items, :last_item
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+
+  def add_item(title, price, quantity = 1)
+    self.total += (price * quantity)
+    quantity.times do
+      self.items.push(title)
+    end
+    self.last_item = (price * quantity)
+  end
+
+  def apply_discount
+    if discount > 0
+    self.total -= self.discount
+    "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+
+
+
+  def void_last_transaction
+    self.total -= self.last_item
+
+  end
+end
 
 # cash = CashRegister.new(20)
 # cashh = CashRegister.new()
